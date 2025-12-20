2d54a46c90159aca58d88355b657727ec3fb06d3
Deric <82902621+dericmg@users.noreply.github.com>
2025-12-20 10:07:45 -0300

    Add files via upload

---
+++ missions.py
@@ -0,0 +1,26 @@
+import os
+import json
+
+appdata_path = os.getenv('APPDATA')
+settings_path = os.path.join(appdata_path, 'discord', 'settings.json')
+new_key = '"DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING": true,'
+
+try:
+    with open(settings_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    file_content = "".join(lines)
+
+    if "DANGEROUS_ENABLE_DEVTOOLS" not in file_content:
+        indentation = "  "
+        new_line = f"{indentation}{new_key}\n"
+        lines.insert(1, new_line)
+
+        with open(settings_path, 'w', encoding='utf-8') as file:
+            file.writelines(lines)
+        print("Line inserted successfully.")
+    else:
+        print("Key already exists in file.")
+
+except Exception as e:
+    print(f"Error: {e}")
